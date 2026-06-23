cba9bc78f3098b6f2638f6942fab5520c7223048
Evgenii Ivanov <ievgesha@mail.ru>
2026-06-23 18:53:03 +0700

    Fix: separation of error types

--- src/Parsers.php
+++ src/Parsers.php
@@ -10,27 +10,37 @@ function parse(array $fileProperties, string $content): array
         'fileName' => "{$fileProperties['fileName']}.{$fileProperties['extension']}",
         'path' => $fileProperties['path'],
     ];
-    $fileData['data'] = match ($fileProperties['extension']) {
-        'json' => parseJson($content),
-        'yaml', 'yml' => parseYaml($content),
-        default => null,
-    };
 
-    if (is_null($fileData['data'])) {
+    try {
+        $fileData['data'] = match ($fileProperties['extension']) {
+            'json' => parseJson($content),
+            'yaml', 'yml' => parseYaml($content),
+            default => throw new \Exception("расширение файла не поддерживается"),
+        };
+    } catch (\Exception $e) {
         $filePath = realpath("{$fileData['path']}/{$fileData['fileName']}");
-        throw new \Exception("'{$filePath}' - некорректный формат содержимого или файл пуст");
+        throw new \Exception("'{$filePath}' - {$e->getMessage()}");
     }
 
     return $fileData;
 }
 
-function parseJson(string $content): object|null
+function parseJson(string $content): object
 {
-    return json_decode($content);
+    $result = json_decode($content);
+    if (is_null($result)) {
+        throw new \Exception("некорректный формат содержимого или файл пуст");
+    }
+
+    return $result;
 }
 
 function parseYaml($content): object|null
 {
     $parsedData = Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
-    return is_object($parsedData) ? $parsedData : null;
+    if (!is_object($parsedData)) {
+        throw new \Exception("некорректный формат содержимого или файл пуст");
+    }
+
+    return $parsedData;
 }
